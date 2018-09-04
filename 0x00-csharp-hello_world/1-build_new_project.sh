867996a85bf4b0080ea59cd3513c2fbc5b0705be
Sid Carroll <squidcarroll@pop-os.localdomain>
2018-09-04 04:51:00 -0600

    remove mkdir statement

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-mkdir 1-new_project
 dotnet new console -o 1-new_project
 dotnet build 1-new_project
--- 0x00-csharp-hello_world/2-run_new_project.sh
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-mkdir 2-new_project
 dotnet new console -o 2-new_project
 dotnet build 2-new_project
 dotnet run -p 2-new_project
