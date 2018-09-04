ac8ca8149d6284f5caaac8b814f12fcd38bd4571
Sid Carroll <squidcarroll@pop-os.localdomain>
2018-09-04 06:09:22 -0600

    add task 9

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: {0}", percent.ToString("p"));
+        Console.WriteLine("Currency: {0}", currency.ToString("c"));
+	}
+}
