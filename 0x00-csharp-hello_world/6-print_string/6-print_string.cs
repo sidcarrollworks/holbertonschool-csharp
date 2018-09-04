84cfe1247edeed2417075a3e24da30697574756e
Sid Carroll <squidcarroll@pop-os.localdomain>
2018-09-04 05:51:03 -0600

    push task 6

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		for (int i = 0; i < 3; i++)
+        {
+            Console.Write(str);
+        }
+        Console.WriteLine();
+        Console.WriteLine(str.Substring(0,9));
+    }
+}
