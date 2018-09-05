997ff392d5b13ee891da5fa3d957c17d4e21bb89
Sid Carroll <squidcarroll@pop-os.localdomain>
2018-09-05 09:03:48 -0600

    push task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char c = 'a'; c <= 'z'; c++)
+                if (c != 'q' && c != 'e')
+                    Console.Write(c);
+        }
+    }
+}
