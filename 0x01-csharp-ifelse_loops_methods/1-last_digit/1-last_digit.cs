2b28c10ade524a2cf1a17a6ad16ad4421ec78d61
Sid Carroll <squidcarroll@pop-os.localdomain>
2018-09-05 08:52:16 -0600

    push task 1

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int last = number % 10;
+
+        Console.Write("The last digit of {0} is {1} ", number, last);
+        if (last > 5)
+        {
+            Console.WriteLine("and is greater than 5");
+        }
+        else if (last is 0) {
+            Console.WriteLine("and is 0");
+        }
+        else if (last < 6 && number != 0) {
+            Console.WriteLine("and is less than 6 and not 0");
+        }
+
+    }
+}
