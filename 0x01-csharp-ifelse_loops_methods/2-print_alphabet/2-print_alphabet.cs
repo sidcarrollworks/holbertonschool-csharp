c38ead7869ad46622a1ac0d200bb6e6822569ab5
Sid Carroll <squidcarroll@pop-os.localdomain>
2018-09-05 08:57:48 -0600

    push task 2

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char c = 'a'; c < 'z'; c++)
+                Console.Write(c);
+        }
+    }
+}
