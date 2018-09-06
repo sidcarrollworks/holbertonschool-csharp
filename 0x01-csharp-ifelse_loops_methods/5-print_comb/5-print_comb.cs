4de56deb3bc1a430503452bfec6cda4ec3127716
squidcarroll <squidcarroll@gmail.com>
2018-09-05 22:52:56 -0700

    push task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("{0}", i.ToString("D2"));
+                if (i < 99) Console.Write(", ");
+            }
+
+        }
+    }
+}
