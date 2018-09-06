0fed787f0ff6e1b84d22dac3f508aaefe4aad6cc
squidcarroll <squidcarroll@gmail.com>
2018-09-05 23:45:39 -0700

    push 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i != j && i < j)
+                    {
+                        Console.Write("{0}{1}", i, j);
+                        if (j != 9 || j - i != 1)
+                            Console.Write(", ");
+                    }
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+}
