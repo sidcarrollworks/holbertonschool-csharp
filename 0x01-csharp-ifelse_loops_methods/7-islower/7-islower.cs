09804a84f7b48666766a35019be09a72f1b5497b
squidcarroll <squidcarroll@gmail.com>
2018-09-06 00:40:21 -0700

    push 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        bool lower = false;
+        for (int i = 'a'; i <= 'z'; i++)
+            if (c == i) lower = true;
+        return lower;
+    }
+}
