894737134fbc2bcb0a9984fcbace6c54d60fdc53
squidcarroll <squidcarroll@gmail.com>
2018-09-06 19:17:43 -0700

    push 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        
+        if (length > 0)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(' ');
+                }
+                Console.Write('\\');
+                Console.Write('\n');
+            }
+        }
+        else
+        Console.WriteLine('\n');
+    }
+}
