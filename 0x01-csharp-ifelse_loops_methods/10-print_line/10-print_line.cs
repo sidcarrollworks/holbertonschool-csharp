50cd0c0856743d283dde2c1f868b48e5865d47b4
squidcarroll <squidcarroll@gmail.com>
2018-09-06 19:12:49 -0700

    push 10

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length < 1)
+        {
+            Console.WriteLine("");
+        }
+        else
+        {
+            string line = new String('_', length);
+            Console.WriteLine(line);
+        }
+    }
+}
