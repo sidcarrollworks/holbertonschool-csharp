8efa2e1fea7d507349a8ec5938c3eed6cbe42586
squidcarroll <squidcarroll@gmail.com>
2018-09-07 21:54:28 -0700

    push 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> result = new List<bool>();
+
+        foreach (var x in myList) {
+            if (x % 2 == 0) {
+                result.Add(true);
+            } else {
+                result.Add(false);
+            }
+        }
+        return result;
+    }
+}
