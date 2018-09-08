60a49b9ced95e252d344bdd340a39589675a5f31
squidcarroll <squidcarroll@gmail.com>
2018-09-07 18:15:21 -0700

    add 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        try {
+                array[index] = Convert.ToInt32(n);
+                return array;
+            } catch (Exception) {
+                Console.WriteLine("Index out of range");
+                return array;
+            }
+    }
+}
