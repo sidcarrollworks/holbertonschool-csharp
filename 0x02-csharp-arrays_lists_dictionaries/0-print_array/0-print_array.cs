a6c2574f397c696e9ac39670b831b217cf0c6c67
squidcarroll <squidcarroll@gmail.com>
2018-09-07 17:58:27 -0700

    push 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0) {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        } else {
+            int[] arr = Enumerable.Range(0, size).ToArray();
+            for (int i = 0; i < size; i++ ) {
+                Console.Write(arr[i].ToString());
+                if ( i < size - 1) {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine("");
+            return arr;
+        }
+        
+    }
+}
