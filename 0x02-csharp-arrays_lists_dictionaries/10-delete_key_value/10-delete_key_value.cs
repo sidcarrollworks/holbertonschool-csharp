44a3405bdde391fc088824280acc343bdee4280f
squidcarroll <squidcarroll@gmail.com>
2018-09-07 22:45:22 -0700

    push 10

---
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+    {
+        try {
+            myDict.Remove(key);
+            return myDict;
+        } catch (Exception) {
+            return myDict;
+        }
+    }
+}
