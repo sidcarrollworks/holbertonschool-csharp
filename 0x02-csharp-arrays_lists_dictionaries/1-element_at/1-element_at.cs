f35d8933885903600ac61f81ab1a5768b85323f0
squidcarroll <squidcarroll@gmail.com>
2018-09-07 18:05:19 -0700

    push 1

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+    class Array
+    {
+        public static int elementAt(int[] array, int index)
+        {
+            try {
+                int element = array[index];
+                return element;
+            } catch (Exception ex) {
+                Console.WriteLine("Index out of range");
+                return (-1);
+            }
+        }
+    }
