eec54769b24e9f16245469cfc8485df6473c1257
squidcarroll <squidcarroll@gmail.com>
2018-09-07 23:11:10 -0700

    push files

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        if (myDict.ContainsKey(key)) {
+            myDict[key] = value;
+        } else {
+            myDict.Add(key, value);
+        }
+        return myDict;
+    }
+}
