e5effe5a1ea914eef8d8c39f73f3e5057a842c58
squidcarroll <squidcarroll@gmail.com>
2018-09-08 23:49:39 -0700

    push 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int bigNum = 0;
+        string name = "None";
+        foreach (KeyValuePair<string, int> x in myList) {
+            if (x.Value > bigNum) {
+                bigNum = x.Value;
+                name = x.Key;
+            }
+        }
+        return name;
+    }
+}
