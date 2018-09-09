b30a0a20142788672956e7ff2dbc50268bb85f53
squidcarroll <squidcarroll@gmail.com>
2018-09-09 00:04:50 -0700

    push files

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> keyList = new List<string>();
+        foreach (KeyValuePair<string, string> y in myDict) {
+            keyList.Add(y.Key);
+        }
+        keyList.Sort();
+        foreach(string x in keyList) {
+            Console.WriteLine("{0}: {1}", x, myDict[x]);
+        }
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[5,5];
+            arr[2,2] = 1;
+            for (int i = 0; i < arr.GetLength(0); i++) {
+                for (int j = 0; j < arr.GetLength(1); j++) {
+                    Console.Write(arr[i, j]);
+                    if (j != arr.GetLength(1) - 1) {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
