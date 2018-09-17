b36182ef74875558d5bc904a49ec8c9a8f88a598
squidcarroll <squidcarroll@gmail.com>
2018-09-17 10:46:05 -0700

    push 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        var set1 = new HashSet<int>(list1);
+        var set2 = new HashSet<int>(list2);
+        
+
+        set1.IntersectWith(set2);
+
+        var newList = new List<int>(set1);
+        
+        return newList;
+    }
+}
