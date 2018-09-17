3a5b0b1c094958984e4ef00d3202f5d71e6d4d81
squidcarroll <squidcarroll@gmail.com>
2018-09-17 11:02:14 -0700

    push 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        var set1 = new SortedSet<int>(list1);
+        var set2 = new SortedSet<int>(list2);
+        
+
+        set1.SymmetricExceptWith(set2);
+
+        var newList = new List<int>(set1);
+        
+        return newList;
+    }
+}
