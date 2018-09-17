85ddf0ff4cc16bfe612e4b8fa3cf082dce61b074
squidcarroll <squidcarroll@gmail.com>
2018-09-17 10:15:19 -0700

    push 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+
+        var unique = new HashSet<int>(myList);
+        foreach(int x in unique) {
+            sum += x;
+        }
+        return sum;
+    }
+}
