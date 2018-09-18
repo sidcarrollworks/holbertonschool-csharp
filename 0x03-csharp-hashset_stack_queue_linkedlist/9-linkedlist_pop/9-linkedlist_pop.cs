570b429c2a72acb1c027d128d3ffece41e43c196
squidcarroll <squidcarroll@gmail.com>
2018-09-17 21:44:05 -0700

    push 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        LinkedList<int>.Enumerator e = myLList.GetEnumerator();
+        e.MoveNext();
+        int value = e.Current;
+        myLList.RemoveFirst();
+        return value;
+    }
+}
