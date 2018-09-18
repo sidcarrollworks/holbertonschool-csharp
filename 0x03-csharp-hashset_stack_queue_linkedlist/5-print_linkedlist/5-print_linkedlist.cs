948e0903553540756af45bfe94cb6a7e9fed502b
squidcarroll <squidcarroll@gmail.com>
2018-09-17 18:20:09 -0700

    add 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> linked = new LinkedList<int>();
+
+        for (int i = 0; i < size; i++) {
+            linked.AddLast(i);
+        }
+
+        foreach (var x in linked) {
+            Console.WriteLine(x);
+        }
+        return linked;
+    }
+}
