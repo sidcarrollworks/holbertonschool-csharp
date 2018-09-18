8c478ff56cd0e5d75077fac1563ff6b8dcf9cdf6
squidcarroll <squidcarroll@gmail.com>
2018-09-17 21:50:44 -0700

    push 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        foreach (int x in myLList) {
+            if (n == 0) {
+                return x;
+            }
+            n--;
+        }
+        return 0;
+    }
+}
