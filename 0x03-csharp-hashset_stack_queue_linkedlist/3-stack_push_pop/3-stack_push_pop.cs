f60ad5ff7daeb5b6c7ccfa822df5ce796bac0f06
squidcarroll <squidcarroll@gmail.com>
2018-09-18 18:19:03 -0700

    push files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string holberton = "NOTREBLOH";
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char x in holberton) {
+            stack.Push(x);
+            Console.Write(x);
+        }
+        Console.WriteLine();
+        
+        Console.Write(stack.Pop());
+        
+    }
+}
