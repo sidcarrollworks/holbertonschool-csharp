2c0e63f41b0faffab10a9aa3f1e40b912c50defd
squidcarroll <squidcarroll@gmail.com>
2018-09-19 10:01:56 -0700

    push 1

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int q = 0;
+        try {
+            q = a / b;
+        } catch (Exception) {
+            Console.WriteLine("Cannot divide by zero");
+        } finally {
+            Console.WriteLine("{0} / {1} = {2}", a, b, q);
+        }
+    }
+}
