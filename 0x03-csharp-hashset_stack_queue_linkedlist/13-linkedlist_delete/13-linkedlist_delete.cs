1ed746e25590fd7493f9646955f37b6cf698a45c
squidcarroll <squidcarroll@gmail.com>
2018-09-18 18:17:54 -0700

    push 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int val = 0;
+        foreach (int x in myLList) {
+            if (index == 0) {
+                val = x;
+            }
+            index--;
+        }
+        LinkedListNode<int> node = myLList.Find(val);
+        myLList.Remove(node);
+    }
+}
