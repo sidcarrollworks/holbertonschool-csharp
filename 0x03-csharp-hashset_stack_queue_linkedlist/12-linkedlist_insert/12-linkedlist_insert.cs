29b9c37cef372bb3cb7ed1401212cf112b742066
squidcarroll <squidcarroll@gmail.com>
2018-09-18 17:53:39 -0700

    push 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        int val = 0;
+        foreach (int x in myLList) {
+            if (x < n) {
+                val = x;
+            }
+        }
+        LinkedListNode<int> node = myLList.Find(val);
+        myLList.AddAfter(node, 21);
+        return node;
+    }
+}
