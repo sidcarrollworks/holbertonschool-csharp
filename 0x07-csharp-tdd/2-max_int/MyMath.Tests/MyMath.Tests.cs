c034f133e42cad8e42397469e8e243491f551234
squidcarroll <squidcarroll@gmail.com>
2018-09-25 16:20:01 -0700

    push 2

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using MyMath;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    public class Tests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            List<int> nums = new List<int>();
+            int max = 0;
+            nums.Add(3);
+            nums.Add(4);
+
+            max = MyMath.Operations.Max(nums);
+
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace MyMath
+{
+    public class Operations
+    {
+        public static int Max(List<int> nums) {
+            if (nums.Any()) {
+                int max = nums.Max();
+                return max;
+            }
+            return 0;
+        }
+    }
+}
