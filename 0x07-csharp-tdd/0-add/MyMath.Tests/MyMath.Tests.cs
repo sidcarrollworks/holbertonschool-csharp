0f5c38db902a6fc33e5a42cbbfda9116ab282e59
squidcarroll <squidcarroll@gmail.com>
2018-09-25 13:19:51 -0700

    push comments

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,14 +1,27 @@
 using NUnit.Framework;
+using MyMath;
 
+/*
+    Test Namespace
+*/
 namespace Tests
 {
+    /// <summary>
+    /// class that holds tests
+    /// </summary>
     public class Tests
     {
+        /// <summary>
+        /// setup for tests
+        /// </summary>
         [SetUp]
         public void Setup()
         {
         }
 
+        /// <summary>
+        /// Tests
+        /// </summary>
         [Test]
         public void Test1()
         {
--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 
+/*
+    Namespace that holds math operations
+*/
 namespace MyMath
 {
+    /// <summary>
+    /// operastions ex add, subtract, multi
+    /// </summary>
     public class Operations
     {
+        /// <summary>
+        /// adds two numbers
+        /// </summary>
+        /// <param name="a">first int</param>
+        /// <param name="b">second int</param>
+        /// <returns>sum of a and b</returns>
         public static int Add(int a, int b) {
             return a + b;
         }
